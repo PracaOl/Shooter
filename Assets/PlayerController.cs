7d7679bf81cdb55f30ff48d4801a4ded22b069f4
PracaOl <oliwer.pietrzycki@op.pl>
2023-02-27 15:17:54 +0100

    Dodaj pliki projektów.

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    Vector2 movementVector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector2 movementVector = Vector2.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * movementVector.x);
+        transform.Translate(Vector3.forward * movementVector.y * Time.deltaTime);
+    }
+    
+    void OnMove(InputValue inputValue)
+    {
+        movementVector = inputValue.Get<Vector2>();
+
+        Debug.Log(movementVector.ToString());
+    }
+}
